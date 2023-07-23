b8e966e02576cd1384b9054ec6c852b81fe173c1
Rokia Darwish <94113862+Rokia-DSC@users.noreply.github.com>
2023-07-23 04:14:51 +0300

    'DB modefied'

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,12 +2,31 @@ const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const { Pool } = require('pg'); //new edit meeting
+
+const express = require('express');//add new user
+const { PrismaClient } = require('@prisma/client');//add new user rokia modified
+
+const prisma = new PrismaClient();//add new user rokia modified
+
+const app = express();//add new user rokia modified
+
+app.get("/" , async (req, res) => {//add new user rokia modified
+  const allUsers = await prisma.UsersTBL.findMany();
+  res.json(allUsers);
+});
+
+app.post("/",async (req,res) =>{//add new user rokia modified
+  const newUser = await prisma.UsersTBL.create({data : req.body});
+  res.json(newUser);
+});
+
 exports.signup = async (req, res, next) => {
-  const fname = req.body.fname;
-  const lname = req.body.lname;
-  const email = req.body.email;
-  const role = req.body.role;
-  const password = req.body.password;
+    const fname = req.body.fname;
+    const lname = req.body.lname; 
+    const email = req.body.email;
+    const role = req.body.role;
+    const password = req.body.password;
   //---------------------------Validations--------------------------
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
