fe2a5a0c4968f6f0a913bfaecd492faa52ac53c4
Mohamed Elsaeed <mohamed.smenshawy@gmail.com>
2023-07-23 07:20:56 +0300

    signup successfully added

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { body } = require("express-validator");
+const { PrismaClient } = require("@prisma/client");
 
 const authController = require("../controllers/auth");
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
 router.put(
   "/signup",
