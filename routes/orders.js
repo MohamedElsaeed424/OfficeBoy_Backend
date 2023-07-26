01bc808d6cd63a9a9fc0da36bb3b2f86edc4bc35
Mohamed Elsaeed <mohamed.smenshawy@gmail.com>
2023-07-26 15:21:54 +0300

    ..

--- routes/orders.js
+++ routes/orders.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const ordersController = require("../controllers/orders");
+const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.post("/order", ordersController.order); //new code
 
-router.get("/orders", ordersController.orders);
+router.get("/orders/:orderId", isAuth, ordersController.getOrders);
 
 module.exports = router;
