bb60e364dd7edb4bc577dade1992532440a9db1c
Mohamed Elsaeed <mohamed.smenshawy@gmail.com>
2023-07-27 06:32:12 +0300

    officeBoy added

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,10 @@ const router = express.Router();
 
 router.post("/add-item", isAuth, adminController.addItem);
 
-router.delete("item/:itemId", isAuth, adminController.deleteItem);
+router.delete("/item/:itemId", isAuth, adminController.deleteItem);
 
-router.put("item/:itemId", isAuth, adminController.updateItem);
+router.put("/item/:itemId", isAuth, adminController.updateItem);
 
-router.get("item/:itemId", isAuth, adminController.getItem);
-
-router.get("item", isAuth, adminController.getNamedItem);
-
-router.get("items", isAuth, adminController.getCategoryItems);
+router.get("/item/:itemId", isAuth, adminController.getItem);
 
 module.exports = router;
