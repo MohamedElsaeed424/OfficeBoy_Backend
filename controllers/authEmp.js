c43c0601ca83a0a0f2f3a1422183b777fcb7d939
Mohamed Elsaeed <mohamed.smenshawy@gmail.com>
2023-08-22 16:43:58 +0300

    kk

--- controllers/authEmp.js
+++ controllers/authEmp.js
@@ -11,50 +11,50 @@ const express = require("express"); //new
 const app = express(); //new
 
 sgMail.setApiKey(
-    "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"
+  "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"
 );
 
 const bathText = path.join(__dirname, "Email_Design.html");
 const emailDesignHtml = markdown.render(bathText);
 
 const catchAsync = (fn) => (req, res, next) => {
-    return Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+  return Promise.resolve(fn(req, res, next)).catch((err) => next(err));
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-    try {
-        const newEmp = await prisma.EmployeeTBL.create({
-            data: {
-                romid: {
-                    connect: {
-                        roomid: req.roomId,
-                    },
-                },
-                offid: {
-                    connect: {
-                        officeid: req.officeId,
-                    },
-                },
-                emp: {
-                    connect: {
-                        userid: req.userId,
-                    },
-                },
-            },
-        });
-        console.log(newEmp);
+  try {
+    const newEmp = await prisma.EmployeeTBL.create({
+      data: {
+        romid: {
+          connect: {
+            roomid: req.roomId,
+          },
+        },
+        offid: {
+          connect: {
+            officeid: req.officeId,
+          },
+        },
+        emp: {
+          connect: {
+            userid: req.userId,
+          },
+        },
+      },
+    });
+    console.log(newEmp);
 
-        res
-            .status(201)
-            // connect with Front end...
-            .json({ message: "User Created Successfully", userId: newUser.userid });
-        sgMail.send({
-            to: email,
-            from: "postman.mord@gmail.com",
-            subject: "Your Signup to postman succeeded!",
-            html: emailDesignHtml,
-        });
-    } catch (err) {
-        throw new Error(400, err.message);
-    }
+    res
+      .status(201)
+      // connect with Front end...
+      .json({ message: "User Created Successfully", userId: newUser.userid });
+    sgMail.send({
+      to: email,
+      from: "postman.mord@gmail.com",
+      subject: "Your Signup to postman succeeded!",
+      html: emailDesignHtml,
+    });
+  } catch (err) {
+    throw new Error(400, err.message);
+  }
 });
